10d6deac785cc2d60b276205457c8f23a8682c83
SKFLAHFLAME <samuelkoh3@gmail.com>
2025-01-16 10:42:59 +0800

    install

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip3 install wheel
+pip3 install dist/*.whl
+
+export PYTHONPATH=$PYTHONPATH:$(pwd)/install/lib/
+sudo bash ./scripts/install_udev_rules.sh
+sudo udevadm control --reload-rules && sudo udevadm trigger
